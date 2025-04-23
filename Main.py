3e46500750c12781892abcbd35c5181c8813dade
Kirill Latyshov <kirilllatyshov99@gmail.com>
2025-04-23 22:06:00 +0300

    Add base parser

--- Main.py
+++ Main.py
@@ -1,28 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D as ax
 import csv
 
-X = []
-Y = []
-Z = []
+Iteration_one = []
+Iteration_multi = []
+Economic_damage_one = []
+Economic_damage_multi = []
+Costs_one = []
+Costs_multi = []
 
 with open('GenResults.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     for row in csvreader:
-        X.append(float(row[0]))
-        Y.append(float(row[1]))
-        Z.append(float(row[2]))
+        Iteration_one.append(float(row[0]))
+        Economic_damage_one.append(float(row[1]))
+        Costs_one.append((float(row[2]) + float(row[3])))
 
+with open('GenResultsMulti.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        Iteration_multi.append(float(row[0]))
+        Economic_damage_multi.append(float(row[1]))
+        Costs_multi.append((float(row[2]) + float(row[3])))
 
-plt.plot(X, Y)
-plt.title('Line Graph using CSV')
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.plot(Iteration_one, Economic_damage_one, label='Экономический ущерб (однокритериальная)')
+plt.plot(Iteration_multi, Economic_damage_multi, label='Экономический ущерб (многокритериальная)')
+plt.title('Изменение экономического ущерба от ненадежности')
+plt.xlabel('Итерация')
+plt.ylabel('Рубли')
+plt.legend(loc="right")
+plt.show()
 
+plt.plot(Iteration_one, Costs_one, label='Затраты (однокритериальная)')
+plt.plot(Iteration_multi, Costs_multi, label='Затраты (многокритериальная)')
+plt.title('Изменение затрат')
+plt.xlabel('Итерация')
+plt.ylabel('Рубли')
+plt.legend(loc="right")
+plt.show()
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot(X, Y, Z, label='parametric curve')
-
+ax = fig.add_subplot(projection='3d')
+ax.plot(Iteration_one, Costs_one, Economic_damage_one, label='parametric curve')
+ax.plot(Iteration_multi, Costs_multi, Economic_damage_multi, label='parametric curve')
 plt.show()
